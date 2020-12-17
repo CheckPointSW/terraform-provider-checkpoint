bd8eaf0e859cc6ed60082189acef03c2d4ae942b
chkp-royl <51701986+chkp-royl@users.noreply.github.com>
2020-12-17 17:16:53 +0200

    Update publish.go

--- commands/publish/publish.go
+++ commands/publish/publish.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/terraform-providers/terraform-provider-checkpoint/commands"
+	"github.com/CheckPointSW/terraform-provider-checkpoint/commands"
 	"os"
 )
 
