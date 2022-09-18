1cf475c86e2540286c3b5fd27a1c639b8f7a5f74
yonatanzar <109988789+yonatanzar@users.noreply.github.com>
2022-09-18 13:12:44 +0300

    add test

---
+++ checkpoint/resource_checkpoint_management_command_get_interfaces_test.go
@@ -0,0 +1,40 @@
+package checkpoint
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"os"
+	"testing"
+)
+
+func TestAccCheckpointManagementGetInterfaces_basic(t *testing.T) {
+
+	commandName := "checkpoint_management_command_get_interfaces.get"
+
+	context := os.Getenv("CHECKPOINT_CONTEXT")
+	if context != "web_api" {
+		t.Skip("Skipping management test")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccManagementGetInterfacesConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(commandName, "name", commandName, "name"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccManagementGetInterfacesConfig() string {
+	return fmt.Sprintf(`
+resource "checkpoint_management_command_get_interfaces" "get" {
+    target_name = "my_gateway"
+}
+`)
+}
