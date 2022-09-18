41d56b0eaa654aa86492f88b75e6733f1bc19b93
yonatanzar <109988789+yonatanzar@users.noreply.github.com>
2022-09-18 13:17:26 +0300

    add test

---
+++ checkpoint/resource_checkpoint_management_command_abort_get_interfaces_test.go
@@ -0,0 +1,44 @@
+package checkpoint
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"os"
+	"testing"
+)
+
+func TestAccCheckpointManagementAbortGetInterfaces_basic(t *testing.T) {
+
+	commandName := "checkpoint_management_command_abort_get_interfaces.abort"
+
+	context := os.Getenv("CHECKPOINT_CONTEXT")
+	if context != "web_api" {
+		t.Skip("Skipping management test")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccManagementAbortGetInterfacesConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(commandName, "name", commandName, "name"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccManagementAbortGetInterfacesConfig() string {
+	return fmt.Sprintf(`
+resource "checkpoint_management_command_get_interfaces" "get" {
+    target_name = "my_gateway"
+}
+
+resource "checkpoint_management_command_abort_get_interfaces" "abort" {
+	task_id = "${checkpoint_management_command_get_interfaces.get.task_id}"
+}
+`)
+}
