69448426a05245f258f8d7b6025b2ce38ba5663a
yonatanzar <109988789+yonatanzar@users.noreply.github.com>
2022-09-22 10:48:36 +0300

    add test

---
+++ checkpoint/data_source_checkpoint_management_tag_test.go
@@ -0,0 +1,48 @@
+package checkpoint
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"os"
+	"testing"
+)
+
+func TestAccDataSourceCheckpointManagementTag_basic(t *testing.T) {
+
+	objName := "tfTestManagementDataAccessLayer_" + acctest.RandString(6)
+	resourceName := "checkpoint_management_tag.tag"
+	dataSourceName := "data.checkpoint_management_tag.data_tag"
+
+	context := os.Getenv("CHECKPOINT_CONTEXT")
+	if context != "web_api" {
+		t.Skip("Skipping management test")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceManagementTagConfig(objName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, "name", resourceName, "name"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccDataSourceManagementTagConfig(name string) string {
+	return fmt.Sprintf(`
+resource "checkpoint_management_tag" "tag" {
+    name = "%s"
+	tags = ["tag1", "tag2"]
+}
+
+data "checkpoint_management_tag" "data_tag" {
+    name = "${checkpoint_management_tag.tag.name}"
+}
+`, name)
+}
