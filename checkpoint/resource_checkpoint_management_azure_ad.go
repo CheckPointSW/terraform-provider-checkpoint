645f19f05c84514726dc229cf38a14a4299c2fb0
yonatanzar <109988789+yonatanzar@users.noreply.github.com>
2022-11-07 17:17:10 +0200

    add import

--- checkpoint/resource_checkpoint_management_azure_ad.go
+++ checkpoint/resource_checkpoint_management_azure_ad.go
@@ -13,6 +13,9 @@ func resourceManagementAzureAd() *schema.Resource {
 		Read:   readManagementAzureAd,
 		Update: updateManagementAzureAd,
 		Delete: deleteManagementAzureAd,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:        schema.TypeString,
