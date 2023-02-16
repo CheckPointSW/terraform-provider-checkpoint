df67e7f88394160024e73764265fdcf6092012ac
roelsieg <roeland.siegers@ah.nl>
2023-02-16 14:06:57 +0100

    removed arrayFlags

--- commands/verify_policy/verify_policy.go
+++ commands/verify_policy/verify_policy.go
@@ -8,19 +8,6 @@ import (
 	"github.com/CheckPointSW/terraform-provider-checkpoint/commands"
 )
 
-type arrayFlags []string
-
-func (i *arrayFlags) String() string {
-	return "my string representation"
-}
-
-func (i *arrayFlags) Set(value string) error {
-	*i = append(*i, value)
-	return nil
-}
-
-var targets arrayFlags
-
 func main() {
 
 	var policyPackage string
