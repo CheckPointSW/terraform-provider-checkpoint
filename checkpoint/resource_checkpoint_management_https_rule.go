703e7b219239c4a9ee2072b9ecd641a6f3d9d14b
Platon Ryzhikov <ihummer63@yandex.ru>
2025-04-04 13:28:39 +0300

    pass layer & id to update call

--- checkpoint/resource_checkpoint_management_https_rule.go
+++ checkpoint/resource_checkpoint_management_https_rule.go
@@ -460,8 +460,18 @@ func updateManagementHttpsRule(d *schema.ResourceData, m interface{}) error {
 	client := m.(*checkpoint.ApiClient)
 	httpsRule := make(map[string]interface{})
 
+    httpsRule["uid"] = d.Id()
+
 	if ok := d.HasChange("rule_number"); ok {
 		httpsRule["rule-number"] = d.Get("rule_number")
+	}
+
+	if ok := d.HasChange("layer"); ok {
+		oldLayer, newLayer := d.GetChange("layer")
+		httpsRule["layer"] = oldLayer
+		httpsRule["new-layer"] = newLayer
+	} else {
+		httpsRule["layer"] = d.Get("layer")
 	}
 
 	if ok := d.HasChange("name"); ok {
